ad39d66eb798c7b106eaa8fd46a991879c575ec1
afadaa <afadaaxxy18@gmail.com>
2019-09-19 12:05:12 +0800

    kategori1

---
+++ database/migrations/2019_09_19_034607_create_kategoris_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKategorisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategoris', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary();
+            $table->string('nama');
+            $table->text('keterangan');
+            $table->timestamp('created_at')->unique();
+            $table->timestamp('updated_at')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->int('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategoris');
+    }
+}
